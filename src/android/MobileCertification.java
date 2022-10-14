9467ae5f307bfb4500024fb9bfa19910d673e2ce
김성준 <ted@bitflow.ai>
2022-10-14 20:13:52 +0900

    오류 수정

--- src/android/MobileCertification.java
+++ src/android/MobileCertification.java
@@ -2,9 +2,9 @@ package org.apache.cordova.niceid;
 
 public class MobileCertification {
 
-    public static String NAME = "mobile_certification";
-    public static String DATA_TYPE = "data_type";
-    public static String PT_NO = "ptNo";
+    public static final String NAME = "mobile_certification";
+    public static final String DATA_TYPE = "data_type";
+    public static final String PT_NO = "ptNo";
 
     public String sName;
     public String sGender;
