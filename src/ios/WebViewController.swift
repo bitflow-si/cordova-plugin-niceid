61521677fda1afe9c73667376735cfe7ff55f661
KimHakSung <82303691+haksung59@users.noreply.github.com>
2023-07-21 10:30:26 +0900

    Update WebViewController.swift
    niceid 본인인증 연동중

--- src/ios/WebViewController.swift
+++ src/ios/WebViewController.swift
@@ -1,29 +1,92 @@
 import UIKit
 import WebKit
 
-
-class WebViewController: UIViewController,WKUIDelegate,WKNavigationDelegate {
-
+class WebViewController: UIViewController,WKNavigationDelegate, WKUIDelegate {
+    
     var webView: WKWebView!
+    var command: CDVInvokedUrlCommand
+    var commandDelegate: CDVCommandDelegate
+    
+    init(command: CDVInvokedUrlCommand, delegate: CDVCommandDelegate) {
+        self.command = command
+        self.commandDelegate = delegate
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        
+        view = webView
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        print(webView.url?.absoluteString ?? "", "decidePolicyFor")
+        
+        // 응답 내용을 가져와서 출력
+//        if let response = navigationResponse.response as? HTTPURLResponse {
+//            print("Response: \(response)")
+//        }
+        
+        // 기본적으로 응답을 허용합니다.
+        decisionHandler(.allow)
+    }
+
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let requestURL = navigationAction.request.url?.absoluteString ?? ""
+        print(requestURL, "WKNavigationAction")
+        
+        if(requestURL.hasPrefix("tauthlink") || requestURL.hasPrefix("ktauthexternalcall") || requestURL.hasPrefix("upluscorporation") || requestURL.hasPrefix("niceipin2")) {
+            decisionHandler(.cancel)
+            return
+        }
+        
+        decisionHandler(.allow)
+        return
+    }
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        let url = URL(string: "https://naver.com")
-        let request = URLRequest(url: url!)
-        webView.configuration.preferences.javaScriptEnabled = true
-        webView.load(request)
         
-    }
-
-    override func loadView() {
+        webView = WKWebView(frame: .zero)
         
-        super.loadView()
-        webView = WKWebView(frame: self.view.frame)
-        webView.uiDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.scrollView.bounces = true
+        webView.scrollView.showsHorizontalScrollIndicator = false
         webView.navigationDelegate = self
-        self.view = self.webView
-    
+        webView.uiDelegate = self
+        webView.scrollView.scrollsToTop = true
+        webView.configuration.suppressesIncrementalRendering = false
+        webView.configuration.selectionGranularity = .dynamic
+        webView.configuration.allowsInlineMediaPlayback = false
+        webView.configuration.allowsAirPlayForMediaPlayback = false
+        webView.configuration.allowsPictureInPictureMediaPlayback = true
+        webView.configuration.websiteDataStore = .default()
+        webView.configuration.mediaTypesRequiringUserActionForPlayback = .all
+        
+        webView.configuration.preferences.minimumFontSize = 0
+        webView.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
+        webView.configuration.preferences.javaScriptEnabled = true
+        
+        view.addSubview(webView)
+        
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        if let url = URL(string: "https://test2-apihealthpilot.snuh.org/view/checkplus"){
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
-
+    
+    
 }
