7ac49c8fa8a049c659ed7640fe99e1bd081d1d0a
KimHakSung <82303691+haksung59@users.noreply.github.com>
2023-07-24 14:28:54 +0900

    Update NiceId.swift

--- src/ios/NiceId.swift
+++ src/ios/NiceId.swift
@@ -2,17 +2,20 @@
     
     @objc(requestNiceId:)
     func requestNiceId(command: CDVInvokedUrlCommand) {
-
-        let webViewController = self.storyboard?.instantiateViewController(withIdentifier: "webViewController") as? WebViewController else { return }
+        
+        let webViewController = WebViewController(command: command, delegate: self.commandDelegate)
         // 화면 전환 애니메이션 설정
         webViewController.modalTransitionStyle = .coverVertical
         // 전환된 화면이 보여지는 방법 설정 (fullScreen)
         webViewController.modalPresentationStyle = .fullScreen
-        self.present(webViewController, animated: true, completion: nil)
+        let rootViewController = self.viewController!
+        rootViewController.present(webViewController, animated: true, completion: nil)
         
         // 플러그인 내용 작성하기
 //        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
 //        print("NiceId::requestNiceId called")
 //        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+        
     }
+            
 }
