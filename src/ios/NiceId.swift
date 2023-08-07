592e46b8a7d2e841facfb36de057c105623a542a
KimHakSung <82303691+haksung59@users.noreply.github.com>
2023-08-07 15:54:35 +0900

    Update NiceId.swift

--- src/ios/NiceId.swift
+++ src/ios/NiceId.swift
@@ -1,27 +1,42 @@
 import Foundation
+import UIKit
+import BackgroundTasks
 
 
 @objc class NiceId : CDVPlugin {
     
+    // var asyncCallbackId: String?
+
     @objc(requestNiceId:)
     func requestNiceId(command: CDVInvokedUrlCommand) {
-        
-        let webViewController = WebViewController(command: command, delegate: self.commandDelegate)
-        
+        // Save the id
+        // asyncCallbackId = command.callbackId
         let args = command.argument(at: 0) as! NSArray
-        print("NiceId::requestNiceId called", args[0], args[1])
+        print("requestNiceId called", args[0], args[1])
+        
+        // Send no result for synchronous callback
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_NO_RESULT)
+        pluginResult?.setKeepCallbackAs(true)
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
         
         // 화면 전환 애니메이션 설정
+        let webViewController = WebViewController(command: command, delegate: self.commandDelegate)
         webViewController.modalTransitionStyle = .coverVertical
-        // 전환된 화면이 보여지는 방법 설정 (fullScreen)
         webViewController.modalPresentationStyle = .fullScreen
         let rootViewController = self.viewController!
         rootViewController.present(webViewController, animated: true, completion: nil)
-        
-        // 플러그인 내용 작성하기
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
-        
+
     }
-            
+
+    // Some async callback
+    /*
+    @objc func didReceiveSomeAsyncResult(_ someResult: String) {
+        if let callbackId = asyncCallbackId {
+            // Send async result
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: someResult)
+            self.commandDelegate.send(pluginResult, callbackId: callbackId)
+            asyncCallbackId = nil
+        }
+    }
+    */
 }
